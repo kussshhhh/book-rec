d3f125a769c95fc73cc56e3f6235fab3d5e126e2
kussshhhh <kushsep21@gmail.com>
2024-08-25 09:26:57 +0530

    data added

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ with open(file_path, 'r', encoding='latin-1') as file:
     for i, row in enumerate(reader):
         if i <= last_processed_row:
             continue
-        if request_count >= 2000:
+        if request_count >= 3000:
             print("daily limit reached, stopping.")
             break
         arr = parse(row)
